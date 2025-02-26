b18df42dfaa98def2bb39285480c42f940c60d66
akram ahil <akram.ahil123@gmail.com>
2025-02-26 12:01:05 +0000

    commit

---
+++ akram-ahil-portfolio/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Experiences from '@/components/Experiences/Experiences'
+
+export default function Home() {
+  return (
+    <>
+    <Experiences/>
+    </>
+  );
+}

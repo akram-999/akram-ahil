03becf9ea80613781f9c98a3edf8c229f9a2fda1
akram ahil <akram.ahil123@gmail.com>
2025-04-01 13:44:43 +0000

    ski

--- akram-ahil-portfolio/src/components/Skills/Skils.jsx
+++ akram-ahil-portfolio/src/components/Skills/Skils.jsx
@@ -1,47 +1,50 @@
 import React from 'react'
-import './Skills.css' // You'll need to create this CSS file
+import './Skills.css'
 
 export default function Skils() {
   const skills = [
-    { name: 'Node.js', icon: '/skils/nodejs.svg', position: { top: '10%', left: '15%' } },
-    { name: 'mongodb', icon: '/skils/mongodb.svg', position: { top: '5%', left: '35%' } },
-    { name: 'photoshop', icon: '/skils/photoshop.svg', position: { top: '20%', left: '45%' } },
-    { name: 'react', icon: '/skils/react.svg', position: { top: '55%', left: '25%' } },
-    { name: 'next', icon: '/skils/nextjs.svg', position: { top: '30%', left: '75%' } },
-    { name: 'postgre', icon: '/skils/postgre.svg', position: { top: '45%', left: '85%' } },
-    { name: 'mysql', icon: '/skils/mysql.svg', position: { top: '40%', left: '65%' } },
-    { name: 'docker', icon: '/skils/docker.svg', position: { top: '70%', left: '65%' } },
-    { name: 'express', icon: '/skils/express.svg', position: { top: '10%', left: '55%' } },
-    { name: 'wordpress', icon: '/skils/wordpress.svg', position: { top: '55%', left: '45%' } },
-    { name: 'javascript', icon: '/skils/javascript.svg', position: { top: '60%', left: '75%' } },
-    { name: 'laravel', icon: '/skils/laravel.svg', position: { top: '45%', left: '55%' } },
-    { name: 'git', icon: '/skils/git.svg', position: { top: '15%', left: '65%' } },
-    { name: 'illustrator', icon: '/skils/illustrator.svg', position: { top: '20%', left: '25%' } },
-    { name: 'ccs3', icon: '/skils/css3.svg', position: { top: '45%', left: '15%' } },
-    { name: 'jwt', icon: '/skils/jwt.svg', position: { top: '75%', left: '15%' } },
-    { name: 'html', icon: '/skils/html5.svg', position: { top: '70%', left: '35%' } },
-    { name: 'ansible', icon: '/skils/ansible.svg', position: { top: '15%', left: '85%' } },
-    { name: 'figma', icon: '/skils/figma.svg', position: { top: '40%', left: '35%' } },
+    { name: 'Node.js', icon: '/skils/nodejs.svg' },
+    { name: 'mongodb', icon: '/skils/mongodb.svg' },
+    { name: 'photoshop', icon: '/skils/photoshop.svg' },
+    { name: 'react', icon: '/skils/react.svg' },
+    { name: 'next', icon: '/skils/nextjs.svg' },
+    { name: 'postgre', icon: '/skils/postgre.svg' },
+    { name: 'mysql', icon: '/skils/mysql.svg' },
+    { name: 'docker', icon: '/skils/docker.svg'},
+    { name: 'express', icon: '/skils/express.svg' },
+    { name: 'wordpress', icon: '/skils/wordpress.svg' },
+    { name: 'javascript', icon: '/skils/javascript.svg'},
+    { name: 'laravel', icon: '/skils/laravel.svg' },
+    { name: 'git', icon: '/skils/git.svg' },
+    { name: 'illustrator', icon: '/skils/illustrator.svg' },
+    { name: 'ccs3', icon: '/skils/css3.svg' },
+    { name: 'jwt', icon: '/skils/jwt.svg' },
+    { name: 'html', icon: '/skils/html5.svg'},
+    { name: 'ansible', icon: '/skils/ansible.svg'},
+    { name: 'figma', icon: '/skils/figma.svg'},
     // Add more skills as needed
+
+    // Add any other skills you want to show
   ]
 
   return (
-    <section className="skills-container">
-      <h1 className='text-5xl font-sans text-center'>My Skills</h1>
-      <div className="skills-wrapper">
-        {skills.map((skill, index) => (
-          <div 
-            key={index} 
-            className="skill-icon"
-            style={{
-              position: 'absolute',
-              top: skill.position.top,
-              left: skill.position.left
-            }}
-          >
-            <img src={skill.icon} alt={skill.name} />
-          </div>
-        ))}
+    <section className="skills-section">
+      <div className="skills-container">
+        <div className="skills-track">
+          {skills.map((skill, index) => (
+            <div key={index} className="skill-item">
+              <img src={skill.icon} alt={skill.name} />
+              <span>{skill.name}</span>
+            </div>
+          ))}
+          {/* Duplicate items for seamless loop */}
+          {skills.map((skill, index) => (
+            <div key={`duplicate-${index}`} className="skill-item">
+              <img src={skill.icon} alt={skill.name} />
+              <span>{skill.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )

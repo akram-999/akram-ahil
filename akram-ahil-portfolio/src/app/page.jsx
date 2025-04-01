39266d1320651f2c189ff4150655dcc75571cb31
akram ahil <akram.ahil123@gmail.com>
2025-04-01 11:31:39 +0000

    Testi

--- akram-ahil-portfolio/src/app/page.jsx
+++ akram-ahil-portfolio/src/app/page.jsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 import Skils from "@/components/Skills/Skils";
-
+import TestimonialsSectionDemo from "@/components/Testimonale/Testimonial";
 
 export default function Home() {
   return (
@@ -9,6 +9,8 @@ export default function Home() {
     <Navbar/>
    <Hero/>
    <Skils/>
+   <TestimonialsSectionDemo/>
     </>
   );
 }
+
